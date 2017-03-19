7a827c38af533bca953770bd0abd6b5672d06ab6
caolei.cl <caolei.cl@alibaba-inc.com>
2017-03-19 12:04:49 +0800

    fix bug

--- src/main/java/com/hackx/spark/SparkStreamDemo.java
+++ src/main/java/com/hackx/spark/SparkStreamDemo.java
@@ -2,7 +2,6 @@ package com.hackx.spark;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.function.Function2;
-import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaPairDStream;
@@ -19,27 +18,25 @@ public class SparkStreamDemo {
 
     public static void main(String[] args) throws Exception {
 
-        SparkConf sparkConf = new SparkConf().setAppName("JavaNetworkWordCount").setMaster("local");
-        JavaStreamingContext ssc = new JavaStreamingContext(sparkConf, Durations.seconds(1));
-        JavaReceiverInputDStream<String> lines = ssc.socketTextStream("localhost", 9999);
+        SparkConf sparkConf = new SparkConf().setAppName("JavaNetworkWordCount").setMaster("local[5]");
+        JavaStreamingContext jssc = new JavaStreamingContext(sparkConf, Durations.seconds(5));
+        JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
 
         JavaDStream<String> words = lines.flatMap(line -> Arrays.asList(SPACE.split(line)).iterator());
-
-        JavaPairDStream<String, Integer> wordCounts = words.mapToPair(
-                new PairFunction<String, String, Integer>() {
-                    @Override
-                    public Tuple2<String, Integer> call(String s) {
-                        return new Tuple2<>(s, 1);
-                    }
-                }).reduceByKey(new Function2<Integer, Integer, Integer>() {
+        JavaPairDStream<String, Integer> wordPairs = words.mapToPair(word -> new Tuple2<>(word, 1));
+//        JavaPairDStream<String, Integer> wordCounts = wordPairs.reduceByKey((x, y) -> x + y);
+        // Reduce function adding two integers, defined separately for clarity
+        Function2<Integer, Integer, Integer> reduceFunc = new Function2<Integer, Integer, Integer>() {
             @Override
             public Integer call(Integer i1, Integer i2) {
                 return i1 + i2;
             }
-        });
+        };
+        // Reduce last 30 seconds of data, every 10 seconds
+        JavaPairDStream<String, Integer> windowedWordCounts = wordPairs.reduceByKeyAndWindow(reduceFunc, Durations.seconds(10), Durations.seconds(5));
+        windowedWordCounts.print();
 
-        wordCounts.print();
-        ssc.start();
-        ssc.awaitTermination();
+        jssc.start();
+        jssc.awaitTermination();
     }
 }
